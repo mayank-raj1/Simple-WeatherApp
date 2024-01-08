ab62ab1fd1508c8ca32ee2e0560e1bfde687df72
Mayank Raj <mayraj035@gmail.com>
2024-01-08 15:29:37 +0530

    Initial Commit

---
+++ Simple-WeatherApp/Simple_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_WeatherAppApp.swift
+//  Simple-WeatherApp
+//
+//  Created by Mayank Raj on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Simple_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
